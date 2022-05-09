babfc652810d73c25a95210a489e2e7e5b4ae65e
Abhishek Manikandan <abhishekmanikan12@gmail.com>
2022-05-09 19:46:47 +0530

    Update Counting_Sort.java

--- Array/Sort/Counting_Sort.java
+++ Array/Sort/Counting_Sort.java
@@ -1,1 +1,37 @@
+package dsandalgo;
 
+public class Main {
+
+    public static void main(String[] args) {
+
+        int[] arr={3,8,2,4,6,5,1,4,9,1};
+
+        CountSort(arr,1,10);  //range
+
+        for(int i=0; i<arr.length;i++)
+        {
+            System.out.print(arr[i]+" ");
+        }
+    }
+
+    public static void CountSort(int [] input, int min, int max)
+    {
+        int[] countArr=new int[(max-min)+1]; //Range of arr
+
+        for(int i=0;i<input.length;i++)
+        {
+            countArr[input[i]-min]++;   //Counter and Incrementer
+        }
+
+        int j=0; //for input array
+
+        for(int i=min;i<=max;i++)      //writing elements to arr based on countArr
+        {
+            while(countArr[i-min]>0)
+            {
+                input[j++]=i;
+                countArr[i-min]--;
+            }
+        }
+    }
+}
