9e80efc46778e2fc20f90effbadf27136b62b95e
Abhishek Manikandan <abhishekmanikan12@gmail.com>
2022-05-30 20:21:16 +0530

    Update Insertion_Sort.java

--- Array/Sort/Insertion_Sort.java
+++ Array/Sort/Insertion_Sort.java
@@ -31,3 +31,47 @@ public class Main {
 
     }
 }
+
+
+//RECURSIVE:
+
+package dsandalgo;
+
+public class Main {
+
+    public static void main(String[] args)
+    {
+
+        int[] arr={20,35,-15,7,55,1,-22};
+
+        Insort(arr, arr.length);
+
+        for(int i=0;i<arr.length;i++)
+        {
+            System.out.print(arr[i]+" ");
+        }
+
+        Insort(arr, arr.length);
+    }
+
+    public static void Insort(int[] input, int unsortedelements)  //Acts as the main 'for' loop in Line 10
+    {
+         if(unsortedelements<2)
+         {
+             return;
+         }
+
+         Insort(input, unsortedelements-1);
+
+         int NewEle=input[unsortedelements-1];
+
+         int i;
+
+         for(i=unsortedelements-1;i>0 && input[i-1]>NewEle;i--)
+         {
+            input[i]=input[i-1];
+         }
+
+         input[i]=NewEle;
+    }
+}
